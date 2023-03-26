dbfd32bf047ccc58b279b7582409a8fde2141a24
ytfdmn <128945195+ytfdmn@users.noreply.github.com>
2023-03-26 09:37:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 默认各参数值，请自行修改.(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 PORT=${PORT:-'8080'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'14872559-aba7-418c-a3e7-9f9b4ec9abb6'}
 WSPATH=${WSPATH:-'argo'}
 
 # 生成 Xray 配置文件
